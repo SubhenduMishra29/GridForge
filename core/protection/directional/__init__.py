f633ad93c60c042cdd2b90ac3a2cceccd56b3099
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2026-08-07 19:04:56 +0530

    Initialize directional protection module

---
+++ core/protection/directional/__init__.py
@@ -0,0 +1,15 @@
+"""
+GridForge Directional Protection
+
+Contains:
+
+- Directional overcurrent logic
+- Forward/reverse fault discrimination
+- Voltage-current angle supervision
+
+"""
+
+
+from core.protection.directional.directional_relay import (
+    DirectionalRelay
+)
