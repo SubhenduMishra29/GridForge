4544ae2b0a764e1eb1a88c920a8192199ae81318
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2026-08-07 17:53:28 +0530

    Refactor ClassicalGenerator to ClassicalMachine

--- dynamics/machine_models.py
+++ dynamics/machine_models.py
@@ -1,37 +1,42 @@
 import numpy as np
-from dynamics.swing_equation import SwingEquation
 
 
-class ClassicalGenerator:
-    def __init__(self, bus, M, Pm, E=1.1):
+class ClassicalMachine:
+
+
+    def __init__(self,
+                 bus,
+                 H,
+                 D,
+                 Pm,
+                 E):
+
         self.bus = bus
-        self.M = M
+
+        self.H = H
+        self.D = D
+
         self.Pm = Pm
         self.E = E
 
-        self.delta = 0.0
-        self.omega = 0.0
 
-        self.swing = SwingEquation(M)
+    def derivatives(self,
+                    delta,
+                    omega,
+                    Pe,
+                    fbase=50):
 
-    def electrical_power(self, V):
-        """
-        Pe = |E||V|/X * sin(delta)
-        Simplified model (X assumed = 1)
-        """
-        return abs(self.E) * abs(V) * np.sin(self.delta)
 
-    def step(self, V, dt):
-        Pe = self.electrical_power(V)
-
-        dδ, dω = self.swing.derivatives(
-            self.delta,
-            self.omega,
-            self.Pm,
-            Pe
+        domega = (
+            np.pi*fbase/self.H *
+            (self.Pm-Pe-self.D*omega)
         )
 
-        self.delta += dδ * dt
-        self.omega += dω * dt
 
-        return self.delta, self.omega
+        ddelta = (
+            2*np.pi*fbase *
+            omega
+        )
+
+
+        return ddelta, domega
