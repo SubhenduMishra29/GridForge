81e21143ce46581040165b1336e4e7304ea3a004
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2026-08-10 08:55:35 +0530

    Add automatic plugin registration in __init__.py
    This file imports all plugins to ensure they register automatically.

---
+++ ui/plugins/__init__.py
@@ -0,0 +1,8 @@
+"""
+Import all plugins so they register automatically.
+"""
+
+from ui.plugins.toolbar_plugin import *
+from ui.plugins.properties_plugin import *
+from ui.plugins.layers_plugin import *
+from ui.plugins.status_plugin import *
