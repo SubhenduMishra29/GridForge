be0d11d132f16893b1cb33e7c38cfcf3ec6603ce
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2026-08-11 21:59:24 +0530

    Initialize __init__.py for dynamic machine plugins

---
+++ plugins/dynamics/machines/__init__.py
@@ -0,0 +1,12 @@
+"""
+GridForge Dynamic Machine Plugins
+=================================
+"""
+
+from .synchronous_machine_4th import (
+    SynchronousMachine4thOrder,
+)
+
+__all__ = [
+    "SynchronousMachine4thOrder",
+]
