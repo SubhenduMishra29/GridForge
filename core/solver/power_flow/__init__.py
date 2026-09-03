7d7ef0a878883e293dcfcc765c1f7eb8ea3de38f
pandaraseswari03-collab <pandaraseswari03@gmail.com>
2026-09-03 13:36:49 +0530

    feat: export power-flow preparation contracts

--- core/solver/power_flow/__init__.py
+++ core/solver/power_flow/__init__.py
@@ -1,16 +1,21 @@
-"""Power-flow numerical contracts and solver components."""
+"""Power-flow numerical contracts, preparation, and solver components."""
 
 from .input import PowerFlowBusType, PowerFlowInput
 from .nr_solver import NewtonRaphsonSolver
+from .preparation import PowerFlowPreparation, PreparedPowerFlow
 from .q_limit_handler import QLimitHandler
 from .result import PowerFlowResult
 from .runtime_state import PowerFlowRuntimeState
 from .solver_options import SolverOptions
 from .sparse_solver import SparseLinearSolver
+from .study_configuration import PowerFlowStudyConfiguration
 
 __all__ = [
     "PowerFlowBusType",
     "PowerFlowInput",
+    "PowerFlowStudyConfiguration",
+    "PowerFlowPreparation",
+    "PreparedPowerFlow",
     "PowerFlowRuntimeState",
     "PowerFlowResult",
     "NewtonRaphsonSolver",
