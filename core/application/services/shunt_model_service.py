42be12d762624d5b3705053b55d1c382a3400a43
madhuri196mishra-cpu <madhuri.196.mishra@gmail.com>
2026-09-09 09:58:02 +0530

    refactor(application): add ShuntModelService boundary

---
+++ core/application/services/shunt_model_service.py
@@ -0,0 +1,8 @@
+"""Application service boundary for Shunt mutations."""
+
+
+class ShuntModelService:
+    """Owns the Application-layer use-case boundary for Shunt mutations."""
+
+
+__all__ = ["ShuntModelService"]
