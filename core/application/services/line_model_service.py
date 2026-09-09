bb6282cc48fd25bac4ffe25a21c1d1a383bf3a49
madhuri196mishra-cpu <madhuri.196.mishra@gmail.com>
2026-09-09 09:58:06 +0530

    refactor(application): add LineModelService boundary

---
+++ core/application/services/line_model_service.py
@@ -0,0 +1,8 @@
+"""Application service boundary for Line mutations."""
+
+
+class LineModelService:
+    """Owns the Application-layer use-case boundary for Line mutations."""
+
+
+__all__ = ["LineModelService"]
