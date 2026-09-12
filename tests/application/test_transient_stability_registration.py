cb413c163338ca4b28f40c7854fd458226710a77
madhuri196mishra-cpu <madhuri.196.mishra@gmail.com>
2026-09-12 16:44:20 +0530

    test: assert registered study type through public service API

--- tests/application/test_transient_stability_registration.py
+++ tests/application/test_transient_stability_registration.py
@@ -5,4 +5,4 @@ from core.network import Network
 def test_application_registers_transient_stability_study():
     application = create_application(Network())
 
-    assert application.study_service.has_handler("transient_stability")
+    assert "transient_stability" in application.study_service.registered_study_types
