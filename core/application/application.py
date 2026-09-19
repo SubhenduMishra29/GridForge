b18f2156530f0775fe1673b125ea844055224ff8
pandaraseswari03-collab <pandaraseswari03@gmail.com>
2026-09-19 13:41:08 +0530

    GF-REMED-20260919-015: centralize topology command vocabulary

--- core/application/application.py
+++ core/application/application.py
@@ -53,16 +53,6 @@ from .validation import ValidationResult
 class Application:
     """Public headless GridForge Application facade."""
 
-    _TOPOLOGY_COMMANDS = frozenset({
-        "model.create_line", "model.delete_line", "model.create_transformer", "model.delete_transformer",
-        "model.create_cable", "model.delete_cable", "model.create_switch", "model.delete_switch",
-        "model.open_switch", "model.close_switch", "model.put_switch_in_service", "model.take_switch_out_of_service",
-        "model.create_disconnector", "model.delete_disconnector", "model.open_disconnector", "model.close_disconnector",
-        "model.put_disconnector_in_service", "model.take_disconnector_out_of_service", "model.create_fuse",
-        "model.delete_fuse", "model.blow_fuse", "model.reset_fuse", "model.put_fuse_in_service",
-        "model.take_fuse_out_of_service", "model.create_breaker", "model.delete_breaker", "model.open_breaker",
-        "model.close_breaker", "model.trip_breaker", "model.put_breaker_in_service", "model.take_breaker_out_of_service",
-    })
     _NETWORK_ELEMENT_CREATE_DELETE_TYPES = frozenset({
         "bus", "line", "cable", "transformer", "switch", "breaker", "disconnector", "fuse", "load",
         "generator", "synchronous_machine", "motor", "shunt", "capacitor", "reactor", "solar", "battery", "grid",
