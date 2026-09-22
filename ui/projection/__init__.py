cf45ea7f66fcbbbb940fe6c1443a4de4f500a9b3
pandaraseswari03-collab <pandaraseswari03@gmail.com>
2026-09-22 07:36:25 +0530

    B39: export generic engineering projection state

--- ui/projection/__init__.py
+++ ui/projection/__init__.py
@@ -7,10 +7,11 @@ from .projection import Projection
 from .projection_adapter import ProjectionAdapter
 from .projection_context import ProjectionContext
 from .projection_registry import ProjectionRegistry
-from .projection_state import ProjectionState
+from .projection_state import EngineeringParameterState, ProjectionState
 from .selection_projection_coordinator import SelectionProjectionCoordinator
 
 __all__ = [
+    "EngineeringParameterState",
     "Projection",
     "ProjectionAdapter",
     "ProjectionContext",
