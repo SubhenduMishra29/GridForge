0c31db3db2a708d4091a6a377be09651db80e359
madhuri196mishra-cpu <madhuri.196.mishra@gmail.com>
2026-09-26 15:00:00 +0530

    GF-CTRL-UI-001 add Control workspace presentation boundary

---
+++ ui/control/ladder/__init__.py
@@ -0,0 +1,6 @@
+"""Ladder presentation adapters."""
+
+from .ladder_projection import LadderProjection
+from .ladder_interaction import LadderInteraction
+
+__all__ = ["LadderProjection", "LadderInteraction"]
