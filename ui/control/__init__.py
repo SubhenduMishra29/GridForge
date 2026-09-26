1899acd0334d03b1aad352e1310b336b87030fbb
madhuri196mishra-cpu <madhuri.196.mishra@gmail.com>
2026-09-26 15:03:01 +0530

    GF-CTRL-UI-001 expose ControlToolRegistry

--- ui/control/__init__.py
+++ ui/control/__init__.py
@@ -1,7 +1,7 @@
 """First-class GridForge V2 Control engineering workspace."""
 
 from .control_workspace import ControlWorkspace
-from .control_tool_palette import ControlToolDescriptor, ControlToolPalette
+from .control_tool_palette import ControlToolDescriptor, ControlToolRegistry, ControlToolPalette
 from .control_inspector import ControlInspector
 from .control_toolbar import ControlToolbar
 from .control_status_bar import ControlStatusBar
@@ -9,6 +9,7 @@ from .control_status_bar import ControlStatusBar
 __all__ = [
     "ControlWorkspace",
     "ControlToolDescriptor",
+    "ControlToolRegistry",
     "ControlToolPalette",
     "ControlInspector",
     "ControlToolbar",
