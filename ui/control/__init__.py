aac670e4fffeec3e61175e41972c85b75543f223
madhuri196mishra-cpu <madhuri.196.mishra@gmail.com>
2026-09-26 14:59:49 +0530

    GF-CTRL-UI-001 add Control workspace presentation boundary

---
+++ ui/control/__init__.py
@@ -0,0 +1,16 @@
+"""First-class GridForge V2 Control engineering workspace."""
+
+from .control_workspace import ControlWorkspace
+from .control_tool_palette import ControlToolDescriptor, ControlToolPalette
+from .control_inspector import ControlInspector
+from .control_toolbar import ControlToolbar
+from .control_status_bar import ControlStatusBar
+
+__all__ = [
+    "ControlWorkspace",
+    "ControlToolDescriptor",
+    "ControlToolPalette",
+    "ControlInspector",
+    "ControlToolbar",
+    "ControlStatusBar",
+]
